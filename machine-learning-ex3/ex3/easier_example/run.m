0531cb7661b2b8acb6451ea3320605e14c1c0a98
michalrakowski@yahoo.com <michalrakowski@yahoo.com>
2017-03-04 05:03:30 +0100

    Add random number generation for the simplified example

--- machine-learning-ex3/ex3/easier_example/run.m
+++ machine-learning-ex3/ex3/easier_example/run.m
@@ -1,39 +1,39 @@
 clear ; close all; clc
 
-X = [7.1 1.2;  % red
-     8 0.9;
-     7.01 1.4;
-     6.01 1.1;
-     
-     4 5;      % green
-     3.9 6;
-     5.5 5.1;
-     4.9 5.2;
-     
-     -3 -1;       %yellow
-     -1.1 -0.9;
-     -1.2 -1.1];
 
+% load the points from files provided
+X = load('dataX.csv');
+y = load('dataY.csv');
+
+
+% plot the points on a scatter plot
+
+plot(X(find(y==0)',1),X(find(y==0)',2),'.r','markerSize',12);
+axis ([-5 12, -11 11]);     
+hold on;
+plot(X(find(y==1)',1),X(find(y==1)',2),'.g','markerSize',12);
+hold on;
+plot(X(find(y==2)',1),X(find(y==2)',2),'.k','markerSize',12);
+
+
+
+% initialize constants
 n = size(X,2);
-     
-plot(X(1:4,1),X(1:4,2),'or')
-hold on;
-plot(X(5:8,1),X(5:8,2),'xg')
-hold on;
-plot(X(9:11,1),X(9:11,2),'oy')
-
-
-X = [ones(size(X,1),1) X];
-y = [0 0 0 0 1 1 1 1 2 2 2]';
-
-lambda=0;
+lambda=1;
 initial_theta = [0 0 0]';
-
 options = optimset('GradObj', 'on', 'MaxIter', 50);
+number_of_classes = 3;
+thetas = zeros(number_of_classes,n+1);
 
-num_labels = 3;
-thetas = zeros(num_labels,n+1);
-for i = 0:num_labels-1
+
+% augment X with a fake property x0, always equal to zero,
+% to make calculations easier
+X = [ones(size(X,1),1) X];
+
+
+% calculate best thetas for each class of points
+% so that later we can use them to make predictions 
+for i = 0:number_of_classes-1
 
     thetas(i+1,:) = fmincg (@(t)(lrCostFunction(t, X, y==i , lambda)),
     initial_theta,
@@ -47,16 +47,32 @@ for i = 0:num_labels-1
 end
 
 
-
-point1 = [2.246 ; -0.2];
-hold on; plot(point1(1), point1(2),'ob');
-point1 = [1; point1];
-probability_of_belonging = round(sigmoid(thetas*point1).*100)./100
-[maximum_value, index_of_max] = max(probability_of_belonging)
-printf("Point belongs to class %d with probability of %f\n", index_of_max,maximum_value);
-
-
+% create a new point and predict in which class it should be put
+%    point1 = [2.246 ; -0.2];
+%    hold on; plot(point1(1), point1(2),'dc');
+%    point1 = [1; point1];
+%    probability_of_belonging = round(sigmoid(thetas*point1).*100)./100;
+%    [maximum_value, index_of_max] = max(probability_of_belonging);
+%    printf("Newly created point belongs to class %d with probability of %f\n", index_of_max,maximum_value);
 
 
-probability_of_belonging = round(sigmoid(thetas*X').*100)./100
-[maximum_value, index_of_max] = max(probability_of_belonging)
+% let's calculate predictions for points we were given as data
+% they should be put to the same catogories they actually belong to
+%    probability_of_belonging = round(sigmoid(thetas*X').*100)./100;
+%    [probability_of_belonging_to_given_class, predicted_class] = max(probability_of_belonging)
+
+
+
+% for a whole group of five points, place them in appropriate classes
+% using the previosly calculated thetas
+how_many_points_to_generate = 10
+X_rand = [ones(how_many_points_to_generate,1) stdnormal_rnd(how_many_points_to_generate,2)*4];
+[probability_of_belonging_to_given_class, predicted_class] = max(round(sigmoid(thetas*X_rand').*100)./100);
+
+
+hold on; plot(X_rand(find(predicted_class==1),2), X_rand(find(predicted_class==1),3),'xr');
+hold on; plot(X_rand(find(predicted_class==2),2), X_rand(find(predicted_class==2),3),'xg');
+hold on; plot(X_rand(find(predicted_class==3),2), X_rand(find(predicted_class==3),3),'xk');
+
+
+[X_rand probability_of_belonging_to_given_class']
